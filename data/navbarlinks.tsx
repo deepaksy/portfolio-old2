a00cd02476a177aff3c9268020313ef4563e0bb6
deepaksy <deepaksy28@gmail.com>
2022-08-15 10:10:30 +0530

    Added Navbar

---
+++ data/navbarlinks.tsx
@@ -0,0 +1,10 @@
+export type NavbarLinksProps={
+    title:string,
+    link:string
+}
+export const navbarlinks:NavbarLinksProps[]=[
+    {title:"About",link:''},
+    {title:"Projects",link:''},
+    {title:"Feeback",link:''}
+    
+]
